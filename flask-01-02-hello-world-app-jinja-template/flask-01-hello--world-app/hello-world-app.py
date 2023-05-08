e000f80249055edbb76e1dcd09e2bd88edbdc264
Efendi <cagliefendi@gmail.com>
2023-05-08 19:16:38 +0300

    test

---
+++ flask-01-02-hello-world-app-jinja-template/flask-01-hello--world-app/hello-world-app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def head():
+    return 'Hello world Efendi'
+
+@app.route('/second')
+def second():
+    return 'This is second page'
+
+@app.route('/third')
+def third():
+    return 'This is third page'
+
+@app.route('/forth/<string:id>')
+def forth(id):
+    return f'Id of this page is {id}'
+
+if __name__ == '__main__':
+    app.run(debug=True)  #local pc de calusırken hataları gormek icin kullanacaz
+    #app.run(host= '0.0.0.0', port=80) # globalde ec2 da calıstırmak icin
