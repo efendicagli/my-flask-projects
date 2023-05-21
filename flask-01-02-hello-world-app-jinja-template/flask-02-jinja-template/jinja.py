f4548e6d5936a32ed5b5c7e90e16413e254fc109
Efendi <cagliefendi@gmail.com>
2023-05-21 20:38:07 +0300

    Portfolio-2 Flask02-03

--- flask-01-02-hello-world-app-jinja-template/flask-02-jinja-template/jinja.py
+++ flask-01-02-hello-world-app-jinja-template/flask-02-jinja-template/jinja.py
@@ -16,5 +16,5 @@ def number():
 
 
 if __name__ == '__main__':
-    #app.run(debug=True, port=3000)
-    app.run(host= '0.0.0.0', port=80)
+     #app.run(debug=True, port=3000)
+    app.run(host='0.0.0.0', port=80)
